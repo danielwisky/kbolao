c4de5061b72d81f92c96c8cb4d1bb57325c9915e
Daniel Wisky <daniel-ddw@hotmail.com>
2022-11-27 21:05:14 -0300

    feat: first commit

---
+++ app/src/test/kotlin/br/com/danielwisky/kbolao/KbolaoApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.danielwisky.kbolao
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KbolaoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
